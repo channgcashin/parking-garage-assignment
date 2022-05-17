8daaf67fdc636a93e36d71061bc35b16b5a2a0aa
channgcashin <71677568+channgcashin@users.noreply.github.com>
2022-05-16 20:29:40 -0400

    second commit

--- park-garage.py
+++ park-garage.py
@@ -1,18 +1,24 @@
 class ParkingGarage():
-    def __init__(self, tickets, parkingSpaces, currentTicket):
-        self.tickets = tickets
-        self.parkingSpaces = parkingSpaces
-        self.currentTicket = currentTicket
+    def __init__(self):
+        self.tickets = [0] * 10
+        self.parkingSpaces = list(range(1,11))
+        self.currentTicket = {'paid' : False}
 
     def takeTicket(self):
-        self.tickets.pop()
-        self.parkingSpaces.pop()
-
+        if self.tickets:
+            self.tickets.pop()
+            print()
+            print("You have parking space", self.parkingSpaces[-1])
+            self.parkingSpaces.pop()
+            print()
+        else:
+            print("There are no parking spaces available! Sorry for the inconvinience")
+        
     def payForParking(self):
         if self.currentTicket['paid'] == False:
             payment = input("Enter an ammount to pay for your parking: ")
 
-            if payment != None:
+            if payment:
                 print("Your ticket has been paid! You have 15 minutes to leave the garage.")
                 self.currentTicket['paid'] = True
             else:
@@ -23,34 +29,30 @@ class ParkingGarage():
     def leaveGarage(self):
         if self.currentTicket['paid'] == True:
             print("Thank you, have a nice day")
+            self.parkingSpaces.append(self.parkingSpaces[-1] + 1)
             self.tickets.append(0)
-            self.parkingSpaces.append(0)
+            self.currentTicket['paid'] = False
         else:
-            payment2 = input("You have yet to pay for your ticket. Enter an ammount to pay for your parking: ")
-            if payment2 != None:
-                print("Thank you, have a nice day")
-                self.tickets.append(0)
-                self.parkingSpaces.append(0)
-            else:
-                print("Insufficient funds. Try Again.")
+            print("You have yet to pay!")
 
-ticks = [0] * 10
-spaces = [0] * 10
-paid = {'paid':False}
-left = False
-thisParkingSpot = ParkingGarage(ticks, spaces, paid)
+garage = ParkingGarage()
 
-take = input("Welcome to our parking garage! Would you like to take a ticket? (Y/N) ")
-lowerTake = take.lower()
-if lowerTake == 'y':
-    thisParkingSpot.takeTicket()
-    while left == False:
-        userInput = input("Enter 1 when you would like to pay for your parking. Enter 2 when you would like to leave the garage. ")
+while True:
+    print()
+    print("Select from the following:")
+    print("1) Take a parking ticket")
+    print("2) Pay for your parking")
+    print("3) Leave the parking garage")
+    print("4) Quit\n")
+    userInput = input("What would you like to do: ")
 
-        if userInput == '1':
-            thisParkingSpot.payForParking()
-        else:
-            thisParkingSpot.leaveGarage()
-            left = True
-else:
-    print("Thank you, have a nice day.")
+    if userInput == '1':
+        garage.takeTicket()
+    elif userInput == '2':
+        garage.payForParking()
+    elif userInput == '3':
+        garage.leaveGarage()
+    elif userInput == '4':
+        break
+    else: 
+        print("Invalid input. Try again.")
